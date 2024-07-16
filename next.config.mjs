725838684f90895dd85d2f12791d65df87b1a0f7
alien-droid <sharma.aditya2796@gmail.com>
2024-07-16 01:30:07 -0500

    updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
     output: "export",  // <=== enables static exports
     reactStrictMode: true,
-    //basePath: "/aditya-portfolio-sharma"
+    basePath: "/aditya-portfolio-sharma"
 };
 
 export default nextConfig;
